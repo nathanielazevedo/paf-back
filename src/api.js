c02d2bb69d3c7afd493f253f993b86ee592079d9
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-08-11 12:52:12 -0400

    met changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL =
+  "https://programafriend.herokuapp.com" || "http://localhost:3001";
 
 
 class Paf {
