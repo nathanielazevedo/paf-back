5ac3d1d2d9848981e967e09fc165ca1f6c415242
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-10-13 07:21:10 -0400

    remove 3d

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,39 +3,9 @@
 import React from "react";
 import "./Home.css";
 import "../../App.css";
-import {Canvas, useLoader, useFrame} from "@react-three/fiber";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
-import {OrbitControls} from "@react-three/drei";
 
 //Basic hero section
 
-function Robot() {
-  const gltf = useLoader(GLTFLoader, "/robot/scene.gltf");
-  useFrame(() => {
-
-    gltf.scene.rotation.y += 0.009
-
-    });
-
-  return (
-    <primitive object={gltf.scene} scale={[20, 20, 20]} position={[0,2,0]}/>
-  );
-}
-
-function ThreeD(){
-  return(
-        <Canvas className="robot"camera={{ position: [5, 0, 0] }} >
-          <OrbitControls
-            enablePan={true}
-            enableZoom={false}
-            enableRotate={true}
-          />
-          {/* <Robot/> */}
-          <ambientLight intensity={0.1}/>
-          <pointLight position={[10, 10, 10]} intensity={0.5} />
-        </Canvas>
-  )
-}
 
 function Home() {
   return (
